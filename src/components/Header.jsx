19e16bbbb9cfac3a5fe77f77047563f4ac5a91dd
nevraka <nevrakaya@gmail.com>
2021-12-06 13:38:20 +0100

    last change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-      <img
-        style={{ width: '100%' }}
-        src="https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"
-        alt="header"
-      />
+      <Link to="/">
+        <img
+          style={{ width: '100%' }}
+          src="https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"
+          alt="header"
+        />
+      </Link>
     </div>
   );
 };
